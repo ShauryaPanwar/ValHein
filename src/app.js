8157509fac1fa958d53ba505d96f1c64bb0a8df9
ShauryaPanwar <panwarshaurya11@gmail.com>
2023-04-18 19:31:27 +0530

    sec

--- src/app.js
+++ src/app.js
@@ -1,17 +1,31 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-
+const hbs=require("hbs");
 require("./db/conn")
 
 
 const port=process.env.PORT || 3000;
 
 const static_path = path.join(__dirname, "../public");
+const views_path = path.join(__dirname, "../templates/views");
+const partial_path = path.join(__dirname, "../templates/partials");
+
+
 app.use(express.static(static_path));
+app.set("view engine", "hbs");
+app.set("views", views_path);
+hbs.registerPartials(partial_path);
+
+
 app.get("/", (req, res) => {
-  res.send("Puzzle Game");
+  res.render("index");
 });
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
 
 
 app.listen(port, () =>{
