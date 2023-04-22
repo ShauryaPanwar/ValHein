5978c72c080078bfba757af4528e9528ee4a20cd
ShauryaPanwar <panwarshaurya11@gmail.com>
2023-04-22 13:36:56 +0530

    comm

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+var cookieParser = require('cookie-parser');  
 const hbs=require("hbs");
 require("./db/conn")
-const Register=require("./models/register")
+const Register=require("./models/register");
+const { log } = require("console");
 
 const port=process.env.PORT || 3000;
 
@@ -20,8 +22,9 @@ app.set("views", views_path);
 hbs.registerPartials(partial_path);
 
 
-
-
+let t0;
+let t1;
+let time;
 
 
 app.get("/", (req, res) => {
@@ -38,10 +41,13 @@ app.get("/story", (req, res) => {
 app.get("/home", (req, res) => {
   res.render("Home");
 });
-
+app.get("/story", (req, res) => {
+  res.render("story");
+});
 
 app.post("/register", async(req, res) => {
   try {
+    const name=req.body.name;
     const password = req.body.pass;
     const confirmpass= req.body.confirmpass;
     if(password===confirmpass){
@@ -54,9 +60,8 @@ app.post("/register", async(req, res) => {
       })
 
       const registered=await rgisterEmployee.save();
-      res.status(201).render("index");
-
-
+      res.status(201).render("Home");
+      t0=performance.now();
     }else{
       res.send("password not matching");
     }
@@ -81,7 +86,10 @@ app.post("/login", async(req, res) => {
       
 
       if(usname.pass===pass){
-        res.status(201).render("index");
+        res.status(201).render("Home");
+        t0=performance.now();
+        
+      
       }else{
         res.send("password not matching");
       }
@@ -90,5 +98,25 @@ app.post("/login", async(req, res) => {
     } 
 });
 
-
+app.post("/story", async(req,res)=>{
+  try {
+    const inp1 = req.body.inp1;
+    const inp2 = req.body.inp2;
+    const inp3 = req.body.inp3;
+    const inp4 = req.body.inp4;
+    const inp5 = req.body.inp5;
+    
+    if(inp1==17 && inp2==19 && inp3==23 && inp4==5 && inp5==71){
+      console.log("Done lessgo");
+      t1=performance.now();
+      time=(t1-t0)/1000;
+      console.log(time);
+      
+    }else{
+      console.log("no");
+    }
+  } catch (error) {
+    res.status(400).send(error);
+  }
+})
 
